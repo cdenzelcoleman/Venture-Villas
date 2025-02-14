6edb427030f6764d4ea4bff08b73ba6b8ed92f5b
naeimsalib <naeimsalib@yahoo.com>
2025-02-14 16:45:40 -0500

    Update setting.py with the DB info

--- ventureVillas/settings.py
+++ ventureVillas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ventureVillas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'venture_villas',
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PW'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': '5432',
     }
 }
 
