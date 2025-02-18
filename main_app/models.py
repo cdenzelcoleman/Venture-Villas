0a7f3fdfeba47af487663a212b188b90114d7813
naeimsalib <naeimsalib@yahoo.com>
2025-02-18 15:02:50 -0500

    Update Model to include Foreign key on Review and Reservations

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+from django.db import models
+from django.urls import reverse
+from datetime import date
+
+from django.contrib.auth.models import User
+
 class Resort(models.Model):
   name = models.CharField(max_length=100)
   location = models.CharField(max_length=100)
@@ -12,7 +18,8 @@ class Resort(models.Model):
     return self.name
   
 class Reservation(models.Model):
-    user = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     resort = models.ForeignKey(Resort, on_delete=models.CASCADE)
     check_in = models.DateField()
     check_out = models.DateField()
@@ -22,7 +29,8 @@ class Reservation(models.Model):
         return f"{self.user} is staying at {self.resort.name} from {self.check_in} to {self.check_out}"
     
 class Review(models.Model):
-    user = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     resort = models.ForeignKey(Resort, on_delete=models.CASCADE)
     rating = models.IntegerField()
     comment = models.TextField()
@@ -30,3 +38,6 @@ class Review(models.Model):
     
     def __str__(self):
         return f"{self.user} rated {self.resort.name} {self.rating} stars"
+    
+class Meta:
+    ordering = ['-date']
