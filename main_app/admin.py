6f335ac4c98cc3ae05f6d55ad4a7997e71f85852
naeimsalib <naeimsalib@yahoo.com>
2025-02-18 16:53:05 -0500

    Update Admin.py to include the models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main_app.models import Resort, Reservation, Review
 
 # Register your models here.
+admin.site.register(Resort)
+admin.site.register(Reservation)
+admin.site.register(Review)
