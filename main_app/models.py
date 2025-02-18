ae711357c2e9490ac07e5cf476c12369b2bb8468
cdenzelcoleman <camdcole@gmail.com>
2025-02-18 11:34:40 -0600

    Add Resort model and fields

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Resort(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  price = models.IntegerField()
+  image = models.CharField(max_length=1000)
+  description = models.TextField()
+  favorite = models.BooleanField(default=False)
+  
+  def __str__(self):
+    return self.name
