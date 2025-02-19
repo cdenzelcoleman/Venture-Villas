d59fec59ebb1e71f212e76a512f4a79ef8252f6d
Dell.O <sherdellmallett@gmail.com>
2025-02-19 12:00:03 -0600

    Add resort preview flexbox

--- ventureVillas/settings.py
+++ ventureVillas/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'ventureVillas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'venture_villas',
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PW'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
