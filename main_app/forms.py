894b3bcb06b6c4550446047f7b556934ad3fded4
cdenzelcoleman <camdcole@gmail.com>
2025-02-20 08:47:15 -0600

    Add Reserve functionality

---
+++ main_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Reservation
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['name', 'check_in', 'check_out']
+        widgets = {
+            'check_in': forms.DateInput(attrs={'type': 'date'}),
+            'check_out': forms.DateInput(attrs={'type': 'date'}),
+        }
